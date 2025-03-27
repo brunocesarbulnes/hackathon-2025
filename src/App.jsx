42424d7482503d2093a6a735ffcf691a7a1f3642
Bruno Bulnes <brunocesar.bulnes@dell.com>
2025-03-27 16:12:01 -0300

    feat: use localstorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { DataChart, DataTable, Footer, Header, Jumbotron, MoneyCard, NewDataModal } from "./components";
 
 export function App() {
   const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   const incoming = data.filter((row) => row.type === "incoming").reduce((acc, row) => acc + parseInt(row.amount), 0);
   const outgoing = data.filter((row) => row.type === "outgoing").reduce((acc, row) => acc + parseInt(row.amount), 0);
@@ -12,21 +13,40 @@ export function App() {
     setData([...data, newData]);
   };
 
+  useEffect(() => {
+    const storedData = window.localStorage.getItem("data");
+    if (storedData) {
+      setData(JSON.parse(storedData));
+    }
+
+    setIsLoading(false);
+  }, []);
+
+  useEffect(() => {
+    if (data.length === 0) return;
+
+    window.localStorage.setItem("data", JSON.stringify(data));
+  }, [data]);
+
   return (
     <>
       <Header />
       <main>
-        <Jumbotron>
-          <MoneyCard title={incoming} subtitle="Incoming" />
-          <MoneyCard title={outgoing} subtitle="Outgoing" />
-          <MoneyCard title={total} subtitle="Total" />
-        </Jumbotron>
+        {!isLoading && (
+          <>
+            <Jumbotron>
+              <MoneyCard title={incoming} subtitle="Incoming" />
+              <MoneyCard title={outgoing} subtitle="Outgoing" />
+              <MoneyCard title={total} subtitle="Total" />
+            </Jumbotron>
 
-        <DataChart />
+            <DataChart />
 
-        <DataTable data={data} />
+            <DataTable data={data} />
 
-        <NewDataModal onFormSubmit={addNewData} />
+            <NewDataModal onFormSubmit={addNewData} />
+          </>
+        )}
       </main>
       <Footer />
     </>
